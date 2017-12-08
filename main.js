476c0dc02c2936068bf0808436345fd7552bb347
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-12-08 15:55:31 -0600

    values being grabbed and added

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
-// Here are the 100 most popular words in English, as totally
-// stolen from here: https://gist.github.com/gravitymonkey/2406023
+// let display = document.querySelector('.display');
+
 var commonWords = [
-  "the","of","and","a","to","in","is","you","that","it","he",
-  "was","for","on","are","as","with","his","they","I","at","be",
-  "this","have","from","or","one","had","by","word","but","not",
-  "what","all","were","we","when","your","can","said","there",
-  "use","an","each","which","she","do","how","their","if","will",
-  "up","other","about","out","many","then","them","these","so",
-  "some","her","would","make","like","him","into","time","has",
-  "look","two","more","write","go","see","number","no","way",
-  "could","people","my","than","first","water","been","call",
-  "who","oil","its","now","find","long","down","day","did","get",
-  "come","made","may","part"
+  "the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he",
+  "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be",
+  "this", "have", "from", "or", "one", "had", "by", "word", "but", "not",
+  "what", "all", "were", "we", "when", "your", "can", "said", "there",
+  "use", "an", "each", "which", "she", "do", "how", "their", "if", "will",
+  "up", "other", "about", "out", "many", "then", "them", "these", "so",
+  "some", "her", "would", "make", "like", "him", "into", "time", "has",
+  "look", "two", "more", "write", "go", "see", "number", "no", "way",
+  "could", "people", "my", "than", "first", "water", "been", "call",
+  "who", "oil", "its", "now", "find", "long", "down", "day", "did", "get",
+  "come", "made", "may", "part"
 ];
 
-// Create a function that choses a random word from `commonWords` and returns it
+
+var counter = 10;
+var triedCharacters = [];
+var correctCharacters = [];
+
 var chooseRandomWord = function(array) {
-    return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 }
 var chosenWord = chooseRandomWord(commonWords);
 
 console.log(chosenWord)
 
 
-var counter = 10;
-var triedCharacters = [];
-var correctCharacters = [];
+
+let character = document.querySelector('#character');
+
+character.addEventListener('keydown', function(event) {
+
+    character = event.target.value
+
+    if(chosenWord === character ){
+      console.log("true")
+    }
+    else{
+      console.log("false")
+  }
+
+
+})
+
 
 // Create a function that accepts a single character argument
 // The function should check the `chosenWord` for that character
@@ -35,11 +53,3 @@ var correctCharacters = [];
 // The function should store and console.log every letter that has been passed to this function in the `triedCharacters` array
 // The function should store and console.log every letter in `chosenWord` that has been passed to this function in the `correctCharacters` array
 // If every letter in `chosenWord` word has been passed to this function, console.log "you guessed it"
-var checkForCharacter = function(character) {
-  for(i=0;i<=character.length;i++){
-
-
-  }
-
-
-}
